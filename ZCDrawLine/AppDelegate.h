cde268643fa60b27d7330fd8cffa55e8a7c2587b
ZiChengLin <260220408@qq.com>
2015-07-03 09:05:06 +0800

    Initial Commit

---
+++ ZCDrawLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCDrawLine
+//
+//  Created by 林梓成 on 15/7/3.
+//  Copyright (c) 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
